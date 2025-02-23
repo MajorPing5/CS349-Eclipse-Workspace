638503d602e55b2eae8749e67080a194646e57fd
Robert-Abel Faustino Porras <majorping5@gmail.com>
2025-02-22 22:54:21 -0600

    Finalized in-class

--- library_project/src/app/app.java
+++ library_project/src/app/app.java
@@ -1,6 +1,7 @@
 package app;
 
 import javax.swing.JOptionPane;
+
 import module.*;
 
 public class app {
@@ -24,11 +25,37 @@ public class app {
         
         // display the book
         library.displayBooks();
+        /**
+         * Could easily create a do...while loop for allowing the user the option to pick
+		 * which operation should be executed.
+		 * 
+		 * Would start with:
+		 * int option = 0;
+		 * do {
+		 * 		option =
+		 * }
+		 * while(option != 3) {
+		 * 		option = JOptionPane.showInputDialog("What would like to do with your library?\n"
+		 * 				+"1. Update a Book\n"
+		 * 				+"2. Remove a Book\n"
+		 * 				+"3. Quit")
+		 * }
+         */
+        //update books
+        updateBook(library);
+        
+        // remove books
+        removeBook(library);
+        
+        System.exit(0);
         
     }
     
     /**
      * create a library and add books
+     * 
+     * could modify to extrapolate book addition into
+	 * its own separate function to allow adding books at any time (either after library creation or after other operations)
      */
     private static Library createLibrary() {
         // create a library
@@ -56,5 +83,73 @@ public class app {
         
         return library;
     }
-
+    
+    /**
+	 * @param library
+	 */
+	private static void updateBook(Library library) {
+		String yesNo;
+		
+		do {
+			int isbn = Integer.parseInt(JOptionPane.showInputDialog("Enter the ISBN to update an entry"));
+			
+			Book bookToUpdate = null;
+			
+			for(Book book: library.getBooks()) {
+				if(book.getISBN() == isbn) {
+					bookToUpdate = book;
+					break;
+				}
+			}
+			if(bookToUpdate != null) {
+				String title = JOptionPane.showInputDialog("Enter new book's title");
+				String author = JOptionPane.showInputDialog("Enter new author's name");
+				
+				bookToUpdate.setTitle(title);
+				bookToUpdate.setAuthor(author);
+				
+				library.displayBooks();
+			} else {
+				JOptionPane.showMessageDialog(null, "Book information doesn't exist in the library");
+			}
+			
+			yesNo = JOptionPane.showInputDialog("Do you have more books to update?\n"
+					+"Enter y for yes or press any key to quit");
+		}
+		while(yesNo.toLowerCase().equals("y"));
+		
+	}
+	
+	/**
+	 * 
+	 * @param library
+	 */
+	private static void removeBook(Library library) {
+		String yesNo;
+		do {
+			// get isbn of the book to be removed
+			String isbn = JOptionPane.showInputDialog(null, "Enter the ISBN");
+			
+			// book object to be removed
+			Book bookToRemove = null;
+			
+			// loop through the books
+			for(Book book: library.getBooks()) {
+				if(book.getISBN() == Integer.parseInt(isbn)) {
+					bookToRemove = book;
+					break;
+				}
+			}
+			if(bookToRemove != null) {
+				library.removeBook(bookToRemove);
+				library.displayBooks();
+			} else {
+				JOptionPane.showMessageDialog(null,  "Book information doesn't exist in the library");
+			}
+			
+			yesNo = JOptionPane.showInputDialog("Do you want to remove another book?\n"
+					+"Enter y to continue or press any key to quit");
+			}
+		while(yesNo.toLowerCase().equals("y"));
+	}
 }
