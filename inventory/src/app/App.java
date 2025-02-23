319e9d74ddb2c4e4fda51c4e14702675bce192e9
Robert-Abel Faustino Porras <majorping5@gmail.com>
2025-02-22 22:44:40 -0600

    background updates w/ inventory project revisions

--- inventory/src/app/App.java
+++ inventory/src/app/App.java
@@ -1,10 +1,13 @@
 package app;
 
+import interaction.*;
+import inventory.*;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.exit(0);
 	}
 
 }
