c7f9ae5601e0e56bb677009d648a97143e2214b9
Robert-Abel Faustino Porras <majorping5@gmail.com>
2025-06-23 10:52:22 -0500

    Full DBMS Integration & Implementation
    Replaces the file-based InventoryModel with InventoryDataAccess, which interacts with a MySQL database for CRUD operations. Adds DatabaseConnection utility for database access, updates controller and repository interfaces, and adapts file-based operations for compatibility. Removes InventoryModel and updates InventoryItem to use double for price. This migration enables persistent storage in a database and prepares for future scalability. Modifies App to allow for localized importing of "inventory.txt" into empty DBMS over 2 rather than 3 separate classes.

--- inventory/src/model/InventoryItem.java
+++ inventory/src/model/InventoryItem.java
@@ -1,32 +1,39 @@
 package model;
 
 public class InventoryItem {
-	
+
 	// fields - ID, Name, Quantity, Price
 	int id, quantity;
 	String name;
-	float price;
-	
+	double price;
+
 	// constructor - empty constructor
 	public InventoryItem() {
-		
 	}
-	
+
 	/**
 	 * Overloaded constructor to retrieve all specified item information
 	 * @param id Integer value of an Item's ID
 	 * @param name String name of the Item
 	 * @param quantity Integer value of an Item's supply count
-	 * @param price Float value of an Item's cost towards consumer
-	 * {@return int id, String name, int quantity, float price}
+	 * @param price double value of an Item's cost towards consumer
+	 * {@return int id, String name, int quantity, double price}
 	 */
-	public InventoryItem(int id, String name, int quantity, float price) {
+	public InventoryItem(int id, String name, int quantity, double price) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
+	public InventoryItem(String name, int quantity, double price) {
+		super();
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
+
 	// setters and getters
 	/**
 	 * @return the id
@@ -73,7 +80,7 @@ public class InventoryItem {
 	/**
 	 * @return the price
 	 */
-	public float getPrice() {
+	public double getPrice() {
 		return price;
 	}
 
@@ -83,7 +90,7 @@ public class InventoryItem {
 	public void setPrice(float price) {
 		this.price = price;
 	}
-	
+
 	/**
 	 * View specific method that only converts float price into string w/ only 2 decimals
 	 * @return the price
